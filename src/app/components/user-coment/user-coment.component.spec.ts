b5db0bdb6fd5a421d6eb59a8575f2199ce5a1976
unknown <197130@ppu.edu.ps>
2022-09-06 20:19:09 +0300

    creat home & detail page

---
+++ src/app/components/user-coment/user-coment.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserComentComponent } from './user-coment.component';
+
+describe('UserComentComponent', () => {
+  let component: UserComentComponent;
+  let fixture: ComponentFixture<UserComentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserComentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserComentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation,  } from '@angular/core';
+import {Swiper, SwiperOptions } from 'swiper';
+import { SwiperComponent } from "swiper/angular";
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
+import { SwiperEvents } from 'swiper/types';
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
+
+
 })
+
 export class HomePageComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    var swiper = new Swiper(".mySwiper", {
+      // spaceBetween: 0,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      loop:true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      // breakpoints:{
+      //     0:{
+      //         slidesPerView:1,
+      //     },
+      //     640:{
+      //         slidesPerView:1,
+      //     },
+      //     768:{
+      //         slidesPerView:1,
+      //     },
+      //     1024:{
+      //         slidesPerView:1,
+      //     },
+      //    }
+    });
+
+    var swiper = new Swiper(".rivew-swiper", {
+      spaceBetween: 20,
+      centeredSlides: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+
+     loop:true,
+     breakpoints:{
+      0:{
+          slidesPerView:1,
+      },
+      640:{
+          slidesPerView:2,
+      },
+      768:{
+          slidesPerView:2,
+      },
+      1024:{
+          slidesPerView:3,
+      },
+     }
+    });
   }
 
 }
