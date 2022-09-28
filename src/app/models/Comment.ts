7a12602e00d49ede52c35e5e867c379e01bf7953
unknown <197130@ppu.edu.ps>
2022-09-28 23:02:01 +0300

    genatate comment and rating classes

---
+++ src/app/models/Comment.ts
@@ -0,0 +1,12 @@
+export interface Comment {
+  id: string,
+  user_id: string,
+  card_id:string,
+  comment :string,
+  userImage:string,
+  rating:number,
+  userName:string,
+  type_name:"Comment",
+
+
+}
