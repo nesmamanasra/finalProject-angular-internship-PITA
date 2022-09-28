c682272ce791257d6a2d9fabbd359dce5f8a8ca0
unknown <197130@ppu.edu.ps>
2022-09-28 18:29:55 +0300

    add Favorite Recipe and favoraite cocktail classes

---
+++ src/app/models/FavoriteC.ts
@@ -0,0 +1,10 @@
+import { Recipe } from './Recipe';
+import { Cocktail } from './Cocktail';
+
+export interface FavoriteC {
+    id: string,
+    user_id: any,
+    type:  Cocktail,
+    type_name:"Cocktail"
+
+}
