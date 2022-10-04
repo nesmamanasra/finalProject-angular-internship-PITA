f525e8fc0e4d0fb8b8dfa52fa6d1733c6a9c31c4
unknown <197130@ppu.edu.ps>
2022-10-04 12:22:42 +0300

    in this commit i have added user card recipe

---
+++ src/app/components/user-recipe/user-recipe.component.ts
@@ -0,0 +1,39 @@
+import { UserRecipe } from './../../models/UserRecipe';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Recipe } from 'src/app/models/Recipe';
+import { CardDataService } from 'src/app/services/card-data.service';
+import { UserDataService } from 'src/app/services/user-data.service';
+
+@Component({
+  selector: 'app-user-recipe',
+  templateUrl: './user-recipe.component.html',
+  styleUrls: ['./user-recipe.component.css'],
+})
+export class UserRecipeComponent implements OnInit {
+  @Input('recipees') recipees?: UserRecipe;
+
+  constructor(
+    public activeRouter: ActivatedRoute,
+    public router: Router,
+    public userdata: UserDataService,
+    public cardser: CardDataService
+  ) {}
+
+  ngOnInit(): void {}
+  navigateToRecipeDetails(recipe: any) {
+    if (recipe) {
+      this.router.navigate([
+        `itemDetail`,
+        { data: JSON.stringify(this.recipees?.type) },
+      ]);
+    }
+  }
+
+  favorate() {
+    this.userdata.addFavorate(this.recipees?.type as Recipe);
+  }
+  deleteRecipe() {
+    this.userdata.deleteRecipe(this.recipees!);
+  }
+}
